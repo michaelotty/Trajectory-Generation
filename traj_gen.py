14820c402fa35596de7e0dea3bc9d8f5d8e72968
Michael Otty <michael.otty@gmail.com>
2020-08-23 11:58:12 +0100

    Reduced glitches, but not for all cases

--- traj_gen.py
+++ traj_gen.py
@@ -59,85 +59,117 @@ t7 = T_V + T_A + T_J
 
 print(f'Time: {t1}, {t2}, {t3}, {t4}, {t5}, {t6}, {t7}')
 
-
+# Create motion path step by step
 s_profile = np.array([])
-v_profile = np.array([])
-a_profile = np.array([])
 
+# Step 1
 t = np.arange(0, t1, TS)
 s1 = 1/6 * J_MAX * t**3
 v1 = 1/2 * J_MAX * t**2
 a1 = J_MAX * t
 s_profile = np.append(s_profile, s1)
-v_profile = np.append(v_profile, v1)
-a_profile = np.append(a_profile, a1)
 pos_ax = plt.subplot(411)
 pos_ax.plot(t, s1, color='black', lw=0.75)
 
+t = t1
+s_end = 1/6 * J_MAX * t**3
+v_end = 1/2 * J_MAX * t**2
+a_end = J_MAX * t
+
+
+# Step 2
 t = np.arange(0, t2 - t1, TS)
-s2 = s_profile[-1] + v_profile[-1] * t + 1/2 * a_profile[-1] * t**2
-v2 = v_profile[-1] + a_profile[-1] * t
-a2 = a_profile[-1] * np.ones_like(s2)
+s2 = s_end + v_end * t + 1/2 * a_end * t**2
+v2 = v_end + a_end * t
+a2 = a_end * np.ones_like(s2)
 s_profile = np.append(s_profile, s2)
-v_profile = np.append(v_profile, v2)
-a_profile = np.append(a_profile, a2)
 pos_ax.plot(t+t1, s2, color='brown', lw=0.75)
 
+t = t2 - t1
+s_end = s_end + v_end * t + 1/2 * a_end * t**2
+v_end = v_end + a_end * t
+# a_end = a_end
+
+
+# Step 3
 t = np.arange(0, t3 - t2, TS)
-s3 = s_profile[-1] + v_profile[-1] * t + 1/2 * a_profile[-1] * t**2 + 1/6 * -J_MAX * t**3
-v3 = v_profile[-1] + a_profile[-1] * t + 1/2 * -J_MAX * t**2
-a3 = a_profile[-1] - J_MAX * t
+s3 = s_end + v_end * t + 1/2 * a_end * t**2 + 1/6 * -J_MAX * t**3
+v3 = v_end + a_end * t + 1/2 * -J_MAX * t**2
+a3 = a_end - J_MAX * t
 s_profile = np.append(s_profile, s3)
-v_profile = np.append(v_profile, v3)
-a_profile = np.append(a_profile, a3)
 pos_ax.plot(t+t2, s3, color='red', lw=0.75)
 
+t = t3 - t2
+s_end = s_end + v_end * t + 1/2 * a_end * t**2 + 1/6 * -J_MAX * t**3
+v_end = v_end + a_end * t + 1/2 * -J_MAX * t**2
+a_end = a_end - J_MAX * t
+
+
+# Step 4
 t = np.arange(0, t4 - t3, TS)
-s4 = s_profile[-1] + v_profile[-1] * t
-v4 = v_profile[-1] * np.ones_like(s4)
+s4 = s_end + v_end * t
+v4 = v_end * np.ones_like(s4)
 a4 = np.zeros_like(s4)
 s_profile = np.append(s_profile, s4)
-v_profile = np.append(v_profile, v4)
-a_profile = np.append(a_profile, a4)
 pos_ax.plot(t+t3, s4, color='orange', lw=0.75)
 
+t = t4 - t3
+s_end = s_end + v_end * t
+v_end = v_end * 1.0
+a_end = 0.0
+
+
+# Step 5
 t = np.arange(0, t5 - t4, TS)
-s5 = s_profile[-1] + v_profile[-1] * t + 1/6 * -J_MAX * t**3
-v5 = v_profile[-1] + 1/2 * -J_MAX * t**2
+s5 = s_end + v_end * t + 1/6 * -J_MAX * t**3
+v5 = v_end + 1/2 * -J_MAX * t**2
 a5 = -J_MAX * t
 s_profile = np.append(s_profile, s5)
-v_profile = np.append(v_profile, v5)
-a_profile = np.append(a_profile, a5)
 pos_ax.plot(t+t4, s5, color='yellow', lw=0.75)
 
+t = t5 - t4
+s_end = s_end + v_end * t + 1/6 * -J_MAX * t**3
+v_end = v_end + 1/2 * -J_MAX * t**2
+a_end = -J_MAX * t
+
+
+# Step 6
 t = np.arange(0, t6 - t5, TS)
-s6 = s_profile[-1] + v_profile[-1] * t + 1/2 * a_profile[-1] * t**2
-v6 = v_profile[-1] + a_profile[-1] * t + 1/2
-a6 = a_profile[-1] * np.ones_like(s6)
+s6 = s_end + v_end * t + 1/2 * a_end * t**2
+v6 = v_end + a_end * t + 1/2
+a6 = a_end * np.ones_like(s6)
 s_profile = np.append(s_profile, s6)
-v_profile = np.append(v_profile, v6)
-a_profile = np.append(a_profile, a6)
 pos_ax.plot(t+t5, s6, color='green', lw=0.75)
 
+t = t6 - t5
+s_end = s_end + v_end * t + 1/2 * a_end * t**2
+v_end = v_end + a_end * t + 1/2
+# a_end = a_end
+
+
+# Step 7
 t = np.arange(0, t7 - t6, TS)
-s7 = s_profile[-1] + v_profile[-1] * t + 1/2 * a_profile[-1] * t**2 + 1/6 * J_MAX * t**3
-v7 = v_profile[-1] + a_profile[-1] * t + 1/2 * J_MAX * t**2
-a7 = a_profile[-1] + J_MAX * t
+s7 = s_end + v_end * t + 1/2 * a_end * t**2 + 1/6 * J_MAX * t**3
+v7 = v_end + a_end * t + 1/2 * J_MAX * t**2
+a7 = a_end + J_MAX * t
 s_profile = np.append(s_profile, s7)
-v_profile = np.append(v_profile, v7)
-a_profile = np.append(a_profile, a7)
 pos_ax.plot(t+t6, s7, color='blue', lw=0.75)
 
-s_profile = direction * np.concatenate((s1[:-1], s2[:-1], s3[:-1], s4[:-1], s5[:-1], s6[:-1], s7))
+
+# Gather Results and Plot
+s_profile = direction * np.concatenate((s1, s2, s3[:-1], s4, s5[:-1], s6, s7[:-3]))
 
 t = np.arange(0, t7-(TS*2), TS)
 vel_ax = plt.subplot(412, sharex=pos_ax)
-vel_ax.plot(t[:-2], np.diff(s_profile)/TS, lw=0.75)
+vel_ax.plot(t[:-1], np.diff(s_profile)/TS, lw=0.75)
 
 acc_ax = plt.subplot(413, sharex=pos_ax)
-acc_ax.plot(t[:-3], np.diff(np.diff(s_profile)/TS)/TS, lw=0.75)
+acc_ax.plot(t[:-2], np.diff(np.diff(s_profile)/TS)/TS, lw=0.75)
 
 jer_ax = plt.subplot(414, sharex=pos_ax)
-jer_ax.plot(t[:-4], np.diff(np.diff(np.diff(s_profile)/TS)/TS)/TS, lw=0.75)
+jer_ax.plot(t[:-3], np.diff(np.diff(np.diff(s_profile)/TS)/TS)/TS, lw=0.75)
+
+plt.figure(2)
+plt.plot(t, s_profile)
 
 plt.show()
