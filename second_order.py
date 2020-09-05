c4daff2073b4bea8ed3b6714baefd923048b4fb6
Michael Otty <michael.otty@gmail.com>
2020-09-05 18:11:07 +0100

    Improved the accuracy of the following

--- second_order.py
+++ second_order.py
@@ -17,41 +17,57 @@ s = 0
 v = 0
 a = 0
 
+last_position_demand = 0.0
+last_velocity_demand = 0.0
+
 for i in range(len(t_range)):
-    if input_demand[i] > s:  # Going positive
-        if np.abs(input_demand[i]-s) <= (v**2/(2*aMax)):
-            a = -aMax
-        else:
-            a = aMax
-    elif input_demand[i] < s:
-        if np.abs(input_demand[i]-s) <= (v**2/(2*aMax)):
-            a = aMax
-        else:
-            a = -aMax
+    input_velocity_demand = (input_demand[i] - last_position_demand)/ts
+
+    if i == 0:
+        final_velocity = input_demand[i] - 0
     else:
-        a = 0
+        final_velocity = input_demand[i] - input_demand[i-1]
+
+    if input_velocity_demand > vMax:
+        input_velocity_demand = vMax
+    elif input_velocity_demand < -vMax:
+        input_velocity_demand = -vMax
+
+    # v^2 = u^2 + 2as
+    # u = +-sqrt(v^2 - 2as)
+
+    sqrt2as = np.sqrt(0.9*np.abs(final_velocity**2 - (2*aMax*(input_demand[i] - last_position_demand))))
+    if input_velocity_demand > sqrt2as:
+        input_velocity_demand = np.sign(input_velocity_demand) * sqrt2as
+
+    input_acceleration_demand = (input_velocity_demand - last_velocity_demand)/ts
+
+    if input_acceleration_demand > aMax:
+        a = aMax
+    elif input_acceleration_demand < -aMax:
+        a = -aMax
+
 
     v = v + a*ts
-    if v > vMax:
-        v = vMax
-    elif v < -vMax:
-        v = -vMax
 
     s = s + v*ts + 0.5*a*ts**2
+    last_position_demand = s
+    last_velocity_demand = v
+
     output_demand = np.append(output_demand, s)
 
 plt.figure(1)
-plt.subplot(311)
-plt.plot(t_range, input_demand, lw=0.75)
-plt.plot(t_range, output_demand, lw=0.75)
+ax1 = plt.subplot(311)
+ax1.plot(t_range, input_demand, lw=0.75)
+ax1.plot(t_range, output_demand, lw=0.75)
 
-plt.subplot(312)
-plt.plot(t_range[1:], np.diff(output_demand)/ts, lw=0.75)
-plt.subplot(313)
-plt.plot(t_range[2:], np.diff(np.diff(output_demand)/ts)/ts, lw=0.75)
+ax2 = plt.subplot(312, sharex=ax1)
+ax2.plot(t_range[1:], np.diff(output_demand)/ts, lw=0.75)
+ax3 = plt.subplot(313, sharex=ax1)
+ax3.plot(t_range[2:], np.diff(np.diff(output_demand)/ts)/ts, lw=0.75)
 
 plt.figure(2)
-plt.plot(t_range[1:], input_demand[1:]-output_demand[:-1], lw=0.75)
+plt.plot(t_range, input_demand-output_demand, lw=0.75)
 plt.axis([t_range[0], t_range[-1], -1, 1])
 
 plt.show()
