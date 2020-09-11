08e7c880bd181562b1ac8ea3fb85729e4a7ce896
Michael Otty <michael.otty@gmail.com>
2020-09-11 18:12:35 +0100

    Corrected some dicontinuities in velocity

--- second_order.py
+++ second_order.py
@@ -1,9 +1,9 @@
-"""Limit demand by second order constraints"""
+"""Limit demand by second order constraints (acceleration and velocity maximum)"""
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-def SymetricalLimit(value, limit, midpoint):
+def symetrical_limit(value, limit, midpoint):
     offset_value = value - midpoint
     limit = abs(limit)
     if offset_value > limit:
@@ -21,27 +21,31 @@ vMax = 80
 
 t_range = np.arange(0, 20, ts)
 
-input_position_demand = -np.arctan2(2500-300*t_range, 1000)*180/np.pi  # np.ones(len(t_range))*60
+options = int(input('Constant (1) or moving target (2): '))
+if options == 1:
+    input_position_demand = np.ones(len(t_range))*60
+else:
+    input_position_demand = np.arctan2(3000-300*t_range, 1000)*180/np.pi
+
+for i in range(len(t_range) - 6000):
+    input_position_demand[i+6000] = 60
+
 input_velocity_demand = np.zeros_like(input_position_demand)
 input_acceleration_demand = np.zeros_like(input_position_demand)
-
 output_demand = np.zeros_like(input_position_demand)
 
 state = {'STOPPED': 0, 'TRACKING': 2, 'SLEWING': 1}
 current_state = state['STOPPED']
+state_array = np.zeros_like(input_position_demand)
 
+# Initialise variables
 s = 0
 v = 0
 a = 0
-
 last_position_demand = 0.0
 last_velocity_demand = 0.0
-
 last_output_position_demand = 0.0
 last_output_velocity_demand = 0.0
-
-fiddle = 1
-decceleration = False
 
 for i in range(len(t_range)):
     input_velocity_demand[i] = (input_position_demand[i] - last_position_demand)/ts
@@ -56,10 +60,10 @@ for i in range(len(t_range)):
     if current_state is state['SLEWING']:
         s = input_position_demand[i] - last_output_position_demand
 
-        v = SymetricalLimit(s/ts, vMax, 0)
-        a = SymetricalLimit(v/ts, aMax, 0)
-        v = SymetricalLimit(last_output_velocity_demand+a*ts, vMax, 0)
-        v = SymetricalLimit(v, np.sqrt(abs(2*a*s)), 0)
+        v = symetrical_limit(s/ts, vMax, 0)
+        a = symetrical_limit(v/ts, aMax, 0)
+        v = symetrical_limit(last_output_velocity_demand+a*ts, vMax, 0)
+        v = symetrical_limit(v, np.sqrt(abs(2*a*s)), 0)
         last_output_velocity_demand = v
         
         s_tracking_limit = input_velocity_demand[i]*ts + 0.5*aMax*ts*ts
@@ -79,7 +83,9 @@ for i in range(len(t_range)):
             current_state = state['TRACKING']
         output_demand[i] = input_position_demand[i]
 
-    
+    state_array[i] = current_state
+    last_output_velocity_demand = (output_demand[i] - last_output_position_demand)/ts
+
     last_output_position_demand = output_demand[i]
 
 plt.figure(1)
@@ -95,9 +101,14 @@ ax3.plot(t_range[2:], np.diff(np.diff(output_demand)/ts)/ts, lw=0.75)
 ax3.axis([t_range[0], t_range[-1], -350, 350])
 
 plt.figure(2)
+plt.subplot(211)
 plt.plot(t_range, input_position_demand-output_demand, lw=0.75)
 plt.axis([t_range[0], t_range[-1], -1, 1])
 plt.title('Error')
+plt.subplot(212)
+plt.plot(t_range, state_array, lw=0.75)
+plt.axis([t_range[0], t_range[-1], -0.1, 2.1])
+
 
 plt.figure(3)
 ax4 = plt.subplot(311)
@@ -109,5 +120,7 @@ ax4.plot(t_range, input_position_demand, lw=0.75)
 
 ax5.plot(t_range, input_velocity_demand, lw=0.75)
 ax6.plot(t_range, input_acceleration_demand, lw=0.75)
+ax5.axis([t_range[0], t_range[-1], -20, 20])
+ax6.axis([t_range[0], t_range[-1], -10, 10])
 
 plt.show()
