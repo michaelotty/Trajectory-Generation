7a9711e66bf3c018b0e7c16e644e00612158bc01
Michael Otty <michael.otty@gmail.com>
2020-09-11 16:58:57 +0100

    Made a sequence that works
    aMax limits are infringed on deccel, but velocity is kept low

--- second_order.py
+++ second_order.py
@@ -3,6 +3,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def SymetricalLimit(value, limit, midpoint):
+    offset_value = value - midpoint
+    limit = abs(limit)
+    if offset_value > limit:
+        offset_value = limit
+    
+    if offset_value < -limit:
+        offset_value = -limit
+
+    return offset_value + midpoint
+
 ts = 1/500.0
 
 aMax = 220
@@ -10,10 +21,14 @@ vMax = 80
 
 t_range = np.arange(0, 20, ts)
 
-input_demand = np.ones(len(t_range))*60#np.arctan2(2500-300*t_range, 1000)*180/np.pi
-actual_velocity_demand = np.diff(input_demand)/ts
-actual_acceleration_demand = np.diff(actual_velocity_demand)/ts
-output_demand = np.array([])
+input_position_demand = -np.arctan2(2500-300*t_range, 1000)*180/np.pi  # np.ones(len(t_range))*60
+input_velocity_demand = np.zeros_like(input_position_demand)
+input_acceleration_demand = np.zeros_like(input_position_demand)
+
+output_demand = np.zeros_like(input_position_demand)
+
+state = {'STOPPED': 0, 'TRACKING': 2, 'SLEWING': 1}
+current_state = state['STOPPED']
 
 s = 0
 v = 0
@@ -21,69 +36,55 @@ a = 0
 
 last_position_demand = 0.0
 last_velocity_demand = 0.0
+
+last_output_position_demand = 0.0
+last_output_velocity_demand = 0.0
+
 fiddle = 1
 decceleration = False
 
 for i in range(len(t_range)):
-    input_velocity_demand = (input_demand[i] - last_position_demand)/ts
-    input_velocity_demand += aMax*ts
+    input_velocity_demand[i] = (input_position_demand[i] - last_position_demand)/ts
+    input_acceleration_demand[i] = (input_velocity_demand[i] - last_velocity_demand)/ts
 
+    last_position_demand = input_position_demand[i]
+    last_velocity_demand = input_velocity_demand[i]
 
-    # s = ut + at^2/2
-    # s = (u + at/2)t
-    # t = s/(u + at/2)
+    if abs(input_velocity_demand[i]) > vMax or abs(input_velocity_demand[i]) > aMax:
+        current_state = state['SLEWING']
 
-    if i == 0:
-        final_velocity = input_demand[i] - 0
-    else:
-        final_velocity = input_demand[i] - input_demand[i-1]
+    if current_state is state['SLEWING']:
+        s = input_position_demand[i] - last_output_position_demand
 
-    if np.abs(input_velocity_demand) > vMax:
-        input_velocity_demand = np.sign(input_velocity_demand) * vMax
+        v = SymetricalLimit(s/ts, vMax, 0)
+        a = SymetricalLimit(v/ts, aMax, 0)
+        v = SymetricalLimit(last_output_velocity_demand+a*ts, vMax, 0)
+        v = SymetricalLimit(v, np.sqrt(abs(2*a*s)), 0)
+        last_output_velocity_demand = v
+        
+        s_tracking_limit = input_velocity_demand[i]*ts + 0.5*aMax*ts*ts
+        v_tracking_limit = np.min([input_velocity_demand[i] + aMax*ts, input_velocity_demand[i] - aMax*ts])
+        if abs(s) < (vMax*ts) and abs(v - input_velocity_demand[i]) < (aMax*ts):
+            current_state = state['TRACKING']
 
-    # v^2 = u^2 + 2as
-    # u^2 = v^2 - 2as
-    # u = +-sqrt(v^2 - 2as)
+        output_demand[i] = last_output_position_demand + v*ts
 
-    if input_demand[i] - last_position_demand > 0.001:
-        ta = np.floor((-(v-final_velocity)+np.sqrt(np.abs((v-final_velocity)**2 - 2*aMax*input_demand[i] - s)))/aMax/ts)*ts
+    if current_state is state['TRACKING']:
+        if last_output_position_demand == input_position_demand[i]:
+            current_state = state['STOPPED']
+        output_demand[i] = input_position_demand[i]
 
-        # sqrt2as = np.sqrt(np.abs(fiddle*2*aMax*(input_demand[i] - last_position_demand) - final_velocity**2))
-        # if np.sqrt(np.abs(final_velocity**2 - fiddle*2*aMax*(input_demand[i] - last_position_demand))) < sqrt2as:
-        #     sqrt2as = np.sqrt(np.abs(final_velocity**2 - fiddle*2*aMax*(input_demand[i] - last_position_demand)))
+    if current_state is state['STOPPED']:
+        if last_output_position_demand != input_position_demand[i]:
+            current_state = state['TRACKING']
+        output_demand[i] = input_position_demand[i]
 
-        # if np.abs(input_velocity_demand) >= sqrt2as:
-        #     input_velocity_demand = np.sign(input_velocity_demand) * sqrt2as
-        if i > 370:
-            print('')
-        if ta <= ts:
-            decceleration = True
-
-        if decceleration:
-            a = -aMax
-        else:
-            input_acceleration_demand = (input_velocity_demand - last_velocity_demand)/ts
-            if np.abs(input_acceleration_demand) > aMax:
-                # a = (input_velocity_demand - last_velocity_demand)/ts
-                a = np.sign(input_acceleration_demand) * aMax
-
-        v += a*ts
-        if np.abs(v) > vMax:
-            v = np.sign(v) * vMax
-
-        tv = (input_demand[i] - s) / v
-
-        s += v*ts + 0.5*a*ts**2
-    else:
-        s = input_demand[i]
-    last_position_demand = s
-    last_velocity_demand = v
-
-    output_demand = np.append(output_demand, s)
+    
+    last_output_position_demand = output_demand[i]
 
 plt.figure(1)
 ax1 = plt.subplot(311)
-ax1.plot(t_range, input_demand, lw=0.75)
+ax1.plot(t_range, input_position_demand, lw=0.75)
 ax1.plot(t_range, output_demand, lw=0.75)
 plt.title('Profiled demand')
 
@@ -94,7 +95,7 @@ ax3.plot(t_range[2:], np.diff(np.diff(output_demand)/ts)/ts, lw=0.75)
 ax3.axis([t_range[0], t_range[-1], -350, 350])
 
 plt.figure(2)
-plt.plot(t_range, input_demand-output_demand, lw=0.75)
+plt.plot(t_range, input_position_demand-output_demand, lw=0.75)
 plt.axis([t_range[0], t_range[-1], -1, 1])
 plt.title('Error')
 
@@ -104,9 +105,9 @@ plt.title('Input Demand')
 ax5 = plt.subplot(312, sharex=ax4)
 ax6 = plt.subplot(313, sharex=ax4)
 
-ax4.plot(t_range, input_demand, lw=0.75)
+ax4.plot(t_range, input_position_demand, lw=0.75)
 
-ax5.plot(t_range[1:], actual_velocity_demand, lw=0.75)
-ax6.plot(t_range[2:], actual_acceleration_demand, lw=0.75)
+ax5.plot(t_range, input_velocity_demand, lw=0.75)
+ax6.plot(t_range, input_acceleration_demand, lw=0.75)
 
 plt.show()
